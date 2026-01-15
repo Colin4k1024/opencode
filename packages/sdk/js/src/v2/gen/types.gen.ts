e4030269d649d10fe64135b21bed18cb3a09ac36
GitHub Action <action@github.com>
2026-01-15 06:36:30 +0000

    chore: generate

--- packages/sdk/js/src/v2/gen/types.gen.ts
+++ packages/sdk/js/src/v2/gen/types.gen.ts
@@ -1536,6 +1536,10 @@ export type ProviderConfig = {
      */
     enterpriseUrl?: string
     /**
+     * Set the OpenAI Responses store flag for this provider
+     */
+    store?: boolean
+    /**
      * Enable promptCacheKey for this provider (default false)
      */
     setCacheKey?: boolean
