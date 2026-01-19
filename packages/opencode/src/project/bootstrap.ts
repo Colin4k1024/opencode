0e5688132defe083b722572a41c5c1cba193cde5
Aiden Cline <63023139+rekram1-node@users.noreply.github.com>
2026-01-18 21:33:23 -0800

    add scheduler, cleanup module (#9346)

--- packages/opencode/src/project/bootstrap.ts
+++ packages/opencode/src/project/bootstrap.ts
@@ -12,6 +12,8 @@ import { Vcs } from "./vcs"
 import { Log } from "@/util/log"
 import { ShareNext } from "@/share/share-next"
 import { init as HookRunnerInit } from "../hook/runner"
+import { Snapshot } from "../snapshot"
+import { Truncate } from "../tool/truncation"
 
 export async function InstanceBootstrap() {
   Log.Default.info("bootstrapping", { directory: Instance.directory })
@@ -24,6 +26,8 @@ export async function InstanceBootstrap() {
   FileWatcher.init()
   File.init()
   Vcs.init()
+  Snapshot.init()
+  Truncate.init()
 
   Bus.subscribe(Command.Event.Executed, async (payload) => {
     if (payload.properties.name === Command.Default.INIT) {
